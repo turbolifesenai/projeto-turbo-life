c4e05e1b070cdc4c22106e3aed46f3e72a78e646
VgBraga <dev.victor.nascimento@gmail.com>
2025-12-05 10:22:55 -0300

    commit 3

---
+++ src/main/java/com/turbolife/projetoturbo/api/entities/Marca.java
@@ -0,0 +1,66 @@
+
+	package com.turbolife.projetoturbo.api.entities;
+
+	import jakarta.persistence.Column;
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.GeneratedValue;
+	import jakarta.persistence.GenerationType;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.Table;
+	import jakarta.validation.constraints.NotBlank;
+
+	@Entity
+	@Table(name = "tb_marcas")
+	public class Marca {
+		
+		@Id
+		@GeneratedValue(strategy= GenerationType.IDENTITY)
+		private Long id;
+
+		@NotBlank(message = "O nome é obrigatório!")
+		@Column(nullable = false)
+		private String nome;
+		
+		@NotBlank(message = "O país de origem é obrigatório!")
+		@Column(nullable = false)
+		private String paisDeOrigem;
+		
+		@Column(name = "logo_url")
+		private String logoUrl;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getPaisDeOrigem() {
+			return paisDeOrigem;
+		}
+
+		public void setPaisDeOrigem(String paisDeOrigem) {
+			this.paisDeOrigem = paisDeOrigem;
+		}
+
+		public String getLogoUrl() {
+			return logoUrl;
+		}
+
+		public void setLogoUrl(String logoUrl) {
+			this.logoUrl = logoUrl;
+		}
+		
+		
+	}
+
+
