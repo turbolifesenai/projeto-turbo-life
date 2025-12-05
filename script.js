ae22df48bd757dca67ddfa50e50b99e3cc777c21
ghmelos <gdemoraismelos@gmail.com>
2025-12-05 12:47:40 -0300

    Marca - HTML

---
+++ script.js
@@ -0,0 +1,12 @@
+function adicionar() {
+
+    const carro = {
+        modelo: document.getElementById("modelo").value,
+        ano: document.getElementById("ano").value,
+        porte: document.getElementById("porte").value,
+        descricao: document.getElementById("descricao").value,
+        capaUrl: document.getElementById("capaUrl").value,
+        id: document.getElementById("paisOrigem").value
+
+    }
+};
